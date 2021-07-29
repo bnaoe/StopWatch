9d461daf6e9d9f4c0ea4459433939d73e3c1c1f2
bnaoe <bp.naoe@gmail.com>
2021-07-28 19:03:03 -0500

    Add project files.

---
+++ StopWatch/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class Calculator
+    {
+        private String _executionInput;
+        public DateTime Start;
+        public DateTime End;
+
+        private TimeSpan _duration;
+
+        public Boolean ValidateInput(String input)
+        {
+            if (input.ToLower() == "start" && string.IsNullOrWhiteSpace(_executionInput)) return true;
+            else if (input.ToLower() == "stop" && _executionInput.ToLower() == "start") return true;
+            else if (_executionInput.ToLower() == "start" && input.ToLower() == "start") return false;
+            else return false;
+
+        }
+
+        public void SetStartEndDuration(DateTime datetime, String input)
+        {
+            _executionInput = input;
+
+            if (_executionInput.ToLower() == "start")
+            {
+                Start = datetime;
+                _executionInput = "start";
+            }
+            else if (_executionInput.ToLower() == "stop")
+            {
+                End = datetime;
+                _duration = End-Start;
+                _executionInput = "";
+            }
+        }
+
+        public TimeSpan GetDuration()
+        {
+            return _duration;
+        }
+        
+    }
+}
---
+++ StopWatch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) // this can execute forever
+            {
+                var calculator = new Calculator();
+
+                Console.WriteLine("Type Start Command (Type: Start): ");
+                var input = Console.ReadLine();
+                if (!calculator.ValidateInput(input)) continue;
+                
+                calculator.SetStartEndDuration(DateTime.Now, input);
+
+                while (!calculator.ValidateInput(input))
+                {
+                    Console.WriteLine("Type End Command (Type: Stop): ");
+                    input = Console.ReadLine();
+                }
+                
+                calculator.SetStartEndDuration(DateTime.Now, input);
+                Console.WriteLine(calculator.GetDuration());
+            }
+        }
+    }
+}
